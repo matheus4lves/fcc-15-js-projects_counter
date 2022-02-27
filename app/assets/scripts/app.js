1e9f32808eee0deea6d6582f2efbec139ef46f24
Matheus Alves <matheusalves.bmd@gmail.com>
2022-02-27 11:52:46 -0300

    Let the user click the buttons to change the counter

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,24 @@
+const value = document.querySelector("#value");
+// DRY principle
+/* If you can use querySelectAll to add an event listener for all buttons,
+why wouldn't you do that? */
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    /* When one of the buttons is clicked, get the classes that style it */
+    const styles = e.currentTarget.classList;
+    // Change the value of count
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    // Update the value of count in the doc
+    value.textContent = count;
+  });
+});
